515b0978bfd00937d9a752e91a973ba4326e109b
Siddharth <74104002+Siddharthsuman@users.noreply.github.com>
2021-01-03 20:02:10 +0530

    Add files via upload

---
+++ sketchnew.js
@@ -0,0 +1,41 @@
+
+var str = "This is a string";
+console.log(str);
+
+var num = 100;
+console.log(num)
+
+var obj;
+console.log(obj);
+
+obj = null;
+console.log(obj);
+
+var arr = [10,20,30];
+console.log(arr);
+
+console.log(arr[0])
+
+var bool = true;
+console.log(bool);
+
+var arr1 = [10, "siddharth", true];
+console.log(arr1);
+
+var arr2 = [[1,2,3],[4,5,6]];
+console.log(arr2);
+
+console.log(arr2[0][2])
+
+arr.push(100);
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+}
+function draw(){
+    background("lightblue");
+}
